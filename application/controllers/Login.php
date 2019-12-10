ce679521d27d62441b1a3361d6b66cda020c9edc
abiel fietus <abiel@terralogiq.com>
2019-12-10 19:37:31 +0700

    fix login api

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -15,7 +15,7 @@ class Login extends API_Controller {
 		$_POST = json_decode(file_get_contents("php://input"), true);
     if ($this->login_validation->index()) {
 			$data = $this->login_model->index();
-			if(array_key_exists('result', $data)){
+			if(array_key_exists('error', $data['result'])){
 				self::response_failed(
 					SELF::HTTP_INTERNAL_ERROR,
 					'Validation error',
@@ -37,7 +37,7 @@ class Login extends API_Controller {
   public function change_password_post(){
     if ($this->login_validation->change_password()) {
 			$data = $this->login_model->change_password();
-			if(array_key_exists('result', $data)){
+			if(array_key_exists('error', $data['result'])){
 				self::response_failed(
 					SELF::HTTP_INTERNAL_ERROR,
 					'Validation error',
